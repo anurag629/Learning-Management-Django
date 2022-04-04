52735b8e405a605054d344c79f6fff5495fa562d
anurag629 <akvermaav629@gmail.com>
2022-04-05 00:25:31 +0530

    crated Answer model

--- users/models.py
+++ users/models.py
@@ -1,3 +1,4 @@
+from pyexpat import model
 from django.db import models
 from django.contrib.auth.models import User
 from PIL import Image
@@ -60,3 +61,12 @@ class Question(models.Model):
 
     def __str__(self):
         return self.question_field
+
+
+class Answer(models.Model):
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    user = models.ForeignKey(Profile, on_delete=models.CASCADE)
+    answer_field = models.CharField(max_length=2000, blank=True, null=True)
+
+    def __str__(self):
+        return f'{self.user} answerd {self.answer_field}'
