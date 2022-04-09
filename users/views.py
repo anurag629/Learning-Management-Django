1fb6e2d7e3d2338d91501a372b5461ee222afebb
anurag629 <akvermaav629@gmail.com>
2022-04-09 09:15:43 +0530

    created register page and register viiew

--- users/views.py
+++ users/views.py
@@ -1,3 +1,22 @@
-from django.shortcuts import render
+from pyexpat.errors import messages
+from django.shortcuts import render, redirect
+
+from users.forms import UserRegisterForm
 
 # Create your views here.
+
+
+def register(request):
+    if request.method == "POST":
+        form = UserRegisterForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.changed_data.get('username')
+            messages.success(
+                request, f'Account created for the {username} successfully!')
+            return redirect('login')
+
+        else:
+            form = UserRegisterForm()
+
+        return render(request, 'user/register.html', {'form': form})
